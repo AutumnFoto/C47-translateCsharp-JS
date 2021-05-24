ea81a7b37d0dedbed42333df988b1490aba53e60
Autumn Fotopoulos <acarte94@gmail.com>
2021-05-24 15:29:39 -0500

    placenames

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,17 @@
-// Put your code here
+const names = [
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
+];
+
+console.log("All Place Names");
+names.forEach((name) => console.log(name));
+
+let thenames = names.filter((name) => name.includes("The"));
+
+thenames.forEach((name) => console.log(`The Place Names ${name}`));
